24a0d805e882788e09ef16d13aa17c5725ff0712
Allan Paz <64302319+Gokuno@users.noreply.github.com>
2023-12-18 18:10:04 -0700

    Update index.js
    Created HTML elements for each card and added the flipcard function.

--- index.js
+++ index.js
@@ -7,3 +7,32 @@ document.addEventListener('DOMContentLoaded', () => {
   
     // Shuffle the card pairs
     const shuffledCards = shuffleArray(cardPairs);
+
+     // Create HTML elements for each card
+    const memoryGame = document.querySelector('.memory-game');
+    shuffledCards.forEach(value => {
+      const card = document.createElement('div');
+      card.classList.add('card');
+      card.dataset.value = value;
+      card.textContent = '?'; // Initial state, cards are face down
+      card.addEventListener('click', () => flipCard(card));
+      memoryGame.appendChild(card);
+    });
+  
+    let flippedCards = [];
+    let matchedPairs = 0;
+  
+    function flipCard(card) {
+      // Prevent flipping more than two cards or clicking on matched cards
+      if (flippedCards.length === 2 || card.classList.contains('matched')) {
+        return;
+      }
+  
+      card.textContent = card.dataset.value; // Reveal the card
+  
+      flippedCards.push(card);
+  
+      if (flippedCards.length === 2) {
+        setTimeout(checkForMatch, 1000);
+      }
+    }
